a9c8245f897d4088521508ecb7649b2102a30fff
akshayshadow321 <163079916+akshayshadow321@users.noreply.github.com>
2024-12-11 16:24:48 +0530

    React routing setup

--- client/my-react-app/src/App.jsx
+++ client/my-react-app/src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import LandingPage from './pages/LandingPage'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-blue-500'> dsahd</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LandingPage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
