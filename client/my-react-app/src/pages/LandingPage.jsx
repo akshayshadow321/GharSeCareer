f295c82dbf1e2b00490191ff9f7485dc68891565
akshayshadow321 <163079916+akshayshadow321@users.noreply.github.com>
2024-12-12 10:56:10 +0530

    minor change in landing page

--- client/my-react-app/src/pages/LandingPage.jsx
+++ client/my-react-app/src/pages/LandingPage.jsx
@@ -3,7 +3,6 @@ import LandingFeatureSection from "../components/LandingFeatureSection"
 import LandingFooter from "../components/LandingFooter"
 import LandingNavbar from "../components/LandingNavbar"
 import { Link } from 'react-router-dom';
-import GetStarted from "../components/GetStarted";
 
 export default function LandingPage() {
     return (
